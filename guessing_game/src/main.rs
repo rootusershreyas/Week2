5eec5cf4bdfdaddb4739024ca5dc8d7417e7aaaa
ShreyasV17103 <vanamalashreyas@gmail.com>
2026-09-10 11:18:29 +0530

    Added a new file `guessing_game/src/main.rs`

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    let mut guess:String = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read input.");
+    println!("You guessed: {}", guess);
+}
