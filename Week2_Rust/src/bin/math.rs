ce1eab569f2708e79f5e8dc07369b4ab55b7c964
ShreyasV17103 <vanamalashreyas@gmail.com>
2026-09-11 11:34:15 +0530

    pushed the file into the repository.

---
+++ Week2_Rust/src/bin/math.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let sum = add(5, 10);
+
+    let difference = subtract(10, 5);
+    
+    println!("The sum is: {}", sum);
+    println!("The difference is: {}", difference);
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn subtract(x: i32, y: i32) -> i32 {
+    x - y
+}
