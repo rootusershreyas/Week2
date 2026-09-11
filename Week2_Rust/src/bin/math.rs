07f682141f4ea52725b31a69700557ed2175e3d8
ShreyasV17103 <vanamalashreyas@gmail.com>
2026-09-11 11:49:16 +0530

    updated path and code did math operations in math.rs file and called it in main.rs file

--- Week2_Rust/src/bin/math.rs
+++ Week2_Rust/src/bin/math.rs
@@ -1,10 +1,13 @@
 fn main() {
     let sum = add(5, 10);
-
     let difference = subtract(10, 5);
-    
-    println!("The sum is: {}", sum);
-    println!("The difference is: {}", difference);
+    let product = multiply(4, 10);
+    let division = quotient(10, 2);
+
+    println!("The sum is: {sum}");
+    println!("The difference is: {difference}");
+    println!("The product is: {product}");
+    println!("The quotient is: {division}");
 }
 
 fn add(x: i32, y: i32) -> i32 {
@@ -13,4 +16,12 @@ fn add(x: i32, y: i32) -> i32 {
 
 fn subtract(x: i32, y: i32) -> i32 {
     x - y
+}
+
+fn multiply(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+fn quotient(x: i32, y: i32) -> i32 {
+    x / y
 }
