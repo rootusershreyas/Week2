b91c1e81079cb12ba905d7f6772759f4593628f1
rootusershreyas <shreyasv@Technoidentity.com>
2026-09-18 15:06:23 +0530

    Pract:Enums and matching

---
+++ Rust_practice/src/bin/final.rs
@@ -0,0 +1,32 @@
+fn main() {
+    //let x = 6;
+    //let y = 3;
+
+    /*let first = String::from("hello");
+    let second = first.clone();
+    println!("{}", second);
+    println!("{}", first); */
+
+    // use x and y so they're not unused
+    //println!("sum: {}", x + y);
+
+    /*let first = String::from("hello");
+    let second = first.clone(); // creates a separate heap copy
+    println!("first = {first}, second = {second}");
+*/
+enum colour{
+    Red,
+    Green,
+    Blue,
+    Violet
+}
+
+let green = colour::Green;
+let blue = colour::Blue;
+let red = colour::Red;
+let violet = colour::Violet;
+
+}
+
+
+
